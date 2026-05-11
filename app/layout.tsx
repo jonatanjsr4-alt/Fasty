c986e27a02b7bb3fc7c90f1643a5881f53bbe71e
Jonatan14 <jonatan.jsr4@gmail.com>
2026-05-11 14:47:42 -0500

    fix pwa metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { CartProvider } from '@/components/CartContext'
 
-export const metadata = {
+export const metadata: Metadata = {
 
   title: 'FASTY',
 
@@ -19,13 +19,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
+
     <html lang="es">
+
       <body>
+
         <CartProvider>
+
           {children}
+
         </CartProvider>
+
       </body>
+
     </html>
+
   )
+
 }
